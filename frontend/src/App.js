ceeeb8401bfcb4c8d7645b76d2ba2f79e35891ae
swtpumpkin <swtpumpkin90@gmail.com>
2018-01-30 13:48:10 +0900

    에러로 인한 초기화

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -6,6 +7,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
