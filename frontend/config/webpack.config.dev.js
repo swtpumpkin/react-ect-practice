c641f55de68dfb9bc47a12da80c086ed897d2ae2
swtpumpkin <swtpumpkin90@gmail.com>
2018-01-30 19:34:12 +0900

    scss모듈 설치 및 webpack 설정

--- frontend/config/webpack.config.dev.js
+++ frontend/config/webpack.config.dev.js
@@ -157,7 +157,7 @@ module.exports = {
           // In production, we use a plugin to extract that CSS to a file, but
           // in development "style" loader enables hot editing of CSS.
           {
-            test: /\.css$/,
+            test: /\.(css|scss)$/,
             use: [
               require.resolve('style-loader'),
               {
@@ -184,7 +184,14 @@ module.exports = {
                       flexbox: 'no-2009',
                     }),
                   ],
+                  sourceMap: true
                 },
+              },
+              {
+                loader: require.resolve('sass-loader'),
+                options: {
+                  sourceMap: true
+                }
               },
             ],
           },
