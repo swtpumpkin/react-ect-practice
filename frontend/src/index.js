e8d38ad735c88eb133921fe517c1c36220030cbc
swtpumpkin <swtpumpkin90@gmail.com>
2018-01-31 14:12:34 +0900

    redux 연결

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import store from './redux/configureStore';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+console.log(store.getState());
 
-localStorage.setItem('instagram', 'practice');
+ReactDOM.render(
+  <Provider store = {store} >
+    <App />
+  </Provider>,
+  document.getElementById('root'));
