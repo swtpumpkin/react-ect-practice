f9a7329e0f274b35ccb83105ff26eb30f8c5098b
swtpumpkin <swtpumpkin90@gmail.com>
2018-01-31 15:03:43 +0900

    react-thunk 설치

--- frontend/src/redux/configureStore.js
+++ frontend/src/redux/configureStore.js
@@ -1,10 +1,14 @@
 import { createStore, combineReducers } from 'redux';
+import thunk from 'redux-thunk';
 import users from 'redux/modules/users';
+import { applyMiddleware } from '../../../../../Library/Caches/typescript/2.6/node_modules/redux';
+
+const middlewares = [thunk];
 
 const reducer = combineReducers({
   users,
 });
 
-let store = initialState => createStore(reducer);
+let store = initialState => createStore(reducer, applyMiddleware(...middlewares));
 
 export default store();
