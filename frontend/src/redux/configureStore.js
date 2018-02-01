011d781f828fe116f7cba4fbc3dd9f721ba17863
swtpumpkin <swtpumpkin90@gmail.com>
2018-02-01 22:39:40 +0900

    redux-logger 모듈 설치 및 dev 모드 설정.

--- frontend/src/redux/configureStore.js
+++ frontend/src/redux/configureStore.js
@@ -3,12 +3,20 @@ import thunk from 'redux-thunk';
 import users from 'redux/modules/users';
 import { applyMiddleware } from '../../../../../Library/Caches/typescript/2.6/node_modules/redux';
 
+const env = process.env.NODE_ENV;
+
 const middlewares = [thunk];
+
+if(env === 'development'){
+  const { logger } = require('redux-logger');
+  middlewares.push(logger);
+}
 
 const reducer = combineReducers({
   users,
 });
 
-let store = initialState => createStore(reducer, applyMiddleware(...middlewares));
+let store = initialState =>
+  createStore(reducer, applyMiddleware(...middlewares));
 
 export default store();
